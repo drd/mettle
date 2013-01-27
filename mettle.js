2c181df5c1d3735bc5e61c6c10250a57ba51c330
Eric O'Connell <eric@zerominuszero.net>
2013-01-26 17:48:18 -0800

    use basic auth to avoid rate limiting; add example user.js in lib

--- mettle.js
+++ mettle.js
@@ -1,4 +1,30 @@
+INTERVAL = 20 * 1000;    // 20 seconds (3600 seconds per hour / 20 = 180 req/hr of 5000)
+
 GHEvents = new Meteor.Collection('ghEvents');
+
+GHEvents.fetchFromServer = function() {
+    var user = User.github.username,
+        pass = User.github.password;
+
+    Meteor.http.get(
+        'https://api.github.com/users/' + user + '/events',
+        { auth: user + ':' + pass },
+        function(error, result) {
+            if (result.statusCode == 200) {
+                _(result.data).each(function(event) {
+                    if (!GHEvents.find({id: event.id}).count()) {
+                        GHEvents.insert(event);
+                    }
+                });
+            }
+
+            console.log(result.headers.status,
+                        result.headers['x-ratelimit-remaining'],
+                        'remaining out of',
+                        result.headers['x-ratelimit-limit']);
+        }
+    );
+};
 
 if (Meteor.is_client) {
     Template.events.ghEvents = function() {
@@ -12,21 +38,8 @@ if (Meteor.is_client) {
 
 if (Meteor.is_server) {
     Meteor.startup(function () {
-        GHEvents.remove({});
+        console.log('mettle is booting up...');
         // code to run on server at startup
-        Meteor.setInterval(function() {
-            Meteor.http.get(
-                'https://api.github.com/users/drd/events',
-                function(error, result) {
-                    if (result.statusCode == 200) {
-                        _(result.data).each(function(event) {
-                            if (!GHEvents.find({id: event.id}).count()) {
-                                GHEvents.insert(event);
-                            }
-                        });
-                    }
-                }
-            );
-        }, 10000);
+        Meteor.setInterval(GHEvents.fetchFromServer, INTERVAL);
     });
 }
